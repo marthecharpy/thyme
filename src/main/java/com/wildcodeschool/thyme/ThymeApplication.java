ff79336b4f319a367683c40f16e2bf263353e2a1
marthe.charpy <marthe.charpy@gmail.com>
2019-01-23 09:46:36 +0100

    first commit

---
+++ src/main/java/com/wildcodeschool/thyme/ThymeApplication.java
@@ -0,0 +1,14 @@
+package com.wildcodeschool.thyme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/wildcodeschool/thyme/ThymeApplicationTests.java
@@ -0,0 +1,17 @@
+package com.wildcodeschool.thyme;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ThymeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
